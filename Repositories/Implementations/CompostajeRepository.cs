4e6beacb74e95ee21ab2272f1243d3fc86474ee8
Mateo Vasquez <e1752779908@uisrael.edu.ec>
2026-06-03 15:47:06 -0500

    Se inicio CompostajeRpository, pero debo acabarlo.

---
+++ Repositories/Implementations/CompostajeRepository.cs
@@ -0,0 +1,40 @@
+﻿using EcoHuellaApp.Data;
+using EcoHuellaApp.Domain.Models;
+using EcoHuellaApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoHuellaApp.Repositories.Implementations
+{
+    public class CompostajeRepository : IRepositoryGeneric<Compostaje>
+    {
+        //instancia de la datbase que debe existir aquí, esto se manda al mauiapp porque... Injección de Dependencias.
+        private AppDatabase _database;
+
+        public Task ActualizarAsync(Compostaje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BorrarRegistroAsync(Compostaje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GuardarRegistroAsync(Compostaje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Compostaje> ObtenerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Compostaje>> ObtenerTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
