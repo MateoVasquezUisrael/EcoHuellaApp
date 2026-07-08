1e6f8463d723919638db741fcad019fd4662b327
MateoVasquezUisrael <e1752779908@uisrael.edu.ec>
2026-07-07 19:35:12 -0500

    Se añadieron nuevas constantes y ecuaciones a MatemáticaVerde

--- Helpers/ConstantesMatematicaVerde.cs
+++ Helpers/ConstantesMatematicaVerde.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace EcoHuellaApp.Helpers;
-    public static class ConstantesMatematicaVerde
-    {
-        //necesario para el cálculo de masa de residuos orgánicos
-        public const double DensidadResiduosOrganicos = 0.30; //contenidos presurtizados de basura orgánica necesarios en el cálculo de masa; se mide en kg/L.
-        public const int VolumenBaldes = 20; //se mide el litros
-    }
+public static class ConstantesMatematicaVerde
+{
+    //necesario para el cálculo de masa de residuos orgánicos
+    public const double DensidadResiduosOrganicos = 0.30; //contenidos presurtizados de basura orgánica necesarios en el cálculo de masa; se mide en kg/L.
+    public const int VolumenBaldes = 20; //se mide el litros
+    public const double CarbonoOrganicoDegradable = 0.15;
+    public const double CarbonoOrganicoDegradableFraccionario = 0.5;
+    public const double FraccionMetanoEnBiogas = 0.5;
+    public const double ConstanteConversionCarbonoMetano = 1.333; //constante de conversión de Caarbono a Metano
+    public const double FactorCorreccionVertedero = 1.0;
+    public const double ConstanteFactorEmisionMetano = 0.004; //equivalente a 4g/kg de Metano por Kg en base a https://www.ipcc-nggip.iges.or.jp/public/2006gl/pdf/5_Volume5/V5_4_Ch4_Bio_Treat.pdf
+}
--- Helpers/MatematicaVerde.cs
+++ Helpers/MatematicaVerde.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using EcoHuellaApp.Helpers;
+using KotlinX.Android.Extensions;
 
 namespace EcoHuellaApp.Helpers
 {
@@ -15,6 +16,25 @@ namespace EcoHuellaApp.Helpers
             /// por Aldea Las Nubes por la densidad calculada dada por la Secrertaria de AMbiente de Quito.
             /// </summary>
             double calculo = cantidadesBaldes * ConstantesMatematicaVerde.VolumenBaldes * ConstantesMatematicaVerde.DensidadResiduosOrganicos;
+
+            return calculo;
+        }
+
+        public double CalcularMetanoEnVertadero(double MasaCarbono)
+        {
+            ///<summary>
+            ///Método simplificado para calcular el potencial de generación de metano de la IPCC
+            ///en https://www.ipcc-nggip.iges.or.jp/public/gp/spanish/5_Waste_ES.pdf
+            /// </summary>
+            double calculo = MasaCarbono * ConstantesMatematicaVerde.CarbonoOrganicoDegradable * ConstantesMatematicaVerde.CarbonoOrganicoDegradableFraccionario
+                * ConstantesMatematicaVerde.FraccionMetanoEnBiogas * ConstantesMatematicaVerde.ConstanteConversionCarbonoMetano * ConstantesMatematicaVerde.FactorCorreccionVertedero;
+
+            return calculo;
+        }
+
+        public double CalcularMetanoEnCompost(double MasaCarbono)
+        {
+            double calculo = MasaCarbono * ConstantesMatematicaVerde.ConstanteFactorEmisionMetano;
 
             return calculo;
         }
