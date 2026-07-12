10412da2b4a3e1d6c347c71baed4e2bda7863cbc
MateoVasquezUisrael <e1752779908@uisrael.edu.ec>
2026-07-11 23:48:31 -0500

    Se añaden entidades finales.

---
+++ Domain/Models/ProcesoComposteraArtesanal/AccioCompostera.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoHuellaApp.Domain.Models.ProcesoComposteraArtesanal
+{
+    internal class EntradaCompostera
+    {
+    }
+}
---
+++ Domain/Models/Ventas/SacosCompost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoHuellaApp.Domain.Models.Ventas
+{
+    internal class SacosCompost
+    {
+    }
+}
